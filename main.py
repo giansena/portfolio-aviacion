2ff04705522e9c6598c687e01d9c2fd9b6cb7c76
Gian Carlo Sena <sena.gian25@gmail.com>
2026-09-03 01:44:22 -0300

    Change upload path to hourly tracking format
    Refactor upload logic to use hourly tracking instead of daily schedule.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Variables de entorno (Configuradas en GitHub Secrets)
+
 API_KEY = os.getenv("AVIATIONSTACK_API_KEY")
 AZURE_CONNECTION_STRING = os.getenv("AZURE_CONNECTION_STRING")
 CONTAINER_NAME = "landing-zone"
@@ -25,30 +25,37 @@ def upload_to_adls(data, folder_path, file_name):
     service_client = DataLakeServiceClient.from_connection_string(AZURE_CONNECTION_STRING)
     file_system_client = service_client.get_file_system_client(file_system=CONTAINER_NAME)
     
-    # Crear ruta de directorio
+
     directory_client = file_system_client.get_directory_client(folder_path)
     if not directory_client.exists():
         directory_client.create_directory()
         
-    # Crear archivo y subir datos
+
     file_client = directory_client.get_file_client(file_name)
     json_data = json.dumps(data)
     file_client.upload_data(json_data, overwrite=True)
     print(f"Archivo guardado en: {folder_path}/{file_name}")
 
 def main():
-    # Obtener datos de EZE y AEP
+  
     flights_eze = get_flights("EZE")
     flights_aep = get_flights("AEP")
     all_flights = flights_eze + flights_aep
     
-    # Mañana (para planificar la flota)
-    tomorrow = datetime.now() + timedelta(days=1)
-    year, month, day = tomorrow.strftime("%Y"), tomorrow.strftime("%m"), tomorrow.strftime("%d")
     
-    # Subir a la ruta particionada
-    folder_path = f"raw/daily_schedule/{year}/{month}/{day}"
-    upload_to_adls(all_flights, folder_path, "schedule.json")
+    now = datetime.utcnow()
+    tomorrow = now + timedelta(days=1)
+    
+    year = tomorrow.strftime("%Y")
+    month = tomorrow.strftime("%m")
+    day = tomorrow.strftime("%d")
+    hour = now.strftime("%H")  
+    
+
+    folder_path = f"raw/hourly_tracking/{year}/{month}/{day}/{hour}"
+    
+
+    upload_to_adls(all_flights, folder_path, "flights_snapshot.json")
 
 if __name__ == "__main__":
     main()
